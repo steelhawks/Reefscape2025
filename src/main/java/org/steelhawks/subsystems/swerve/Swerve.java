da2503dd0b2d6cda4301679b2830a3f0c8266af5
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2025-02-22 14:13:37 -0500

    Update Omega Weight

--- src/main/java/org/steelhawks/subsystems/swerve/Swerve.java
+++ src/main/java/org/steelhawks/subsystems/swerve/Swerve.java
@@ -113,7 +113,7 @@ public class Swerve extends SubsystemBase {
                         Math.max(
                             Math.hypot(TunerConstants.BackLeft.LocationX, TunerConstants.BackLeft.LocationY),
                             Math.hypot(TunerConstants.BackRight.LocationX, TunerConstants.BackRight.LocationY)));
-                ROBOT_MASS_KG = Units.lbsToKilograms(51.2);
+                ROBOT_MASS_KG = Units.lbsToKilograms(115.2);
                 ROBOT_MOI = (1.0 / 12.0) * ROBOT_MASS_KG * (2 * Math.pow(25, 2));
                 WHEEL_COF = COTS.WHEELS.COLSONS.cof;
                 PP_CONFIG =
