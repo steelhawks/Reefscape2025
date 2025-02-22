646629e227f50c7c6fd0ebaf447c13acc207133a
Freddy-Y <ignisavem99@gmail.com>
2025-02-22 00:41:23 -0500

    removed led shallow climb trigger

--- src/main/java/org/steelhawks/RobotContainer.java
+++ src/main/java/org/steelhawks/RobotContainer.java
@@ -326,10 +326,6 @@ public class RobotContainer {
             .onTrue(
                 s_LED.flashCommand(LEDColor.BLUE, 0.1, 1));
 
-        s_Climb.atOuterLimit()
-            .onTrue(
-                s_LED.flashCommand(LEDColor.HOT_PINK, 0.1, 1));
-
         isShallowEndgame
             .onTrue(
                 Commands.runOnce(() ->
