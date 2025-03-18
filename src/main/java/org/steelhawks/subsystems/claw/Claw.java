f6817817ad1f2766ac564d59f3bcfb6f4ba48d99
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2025-03-18 12:55:21 -0400

    claw canrange changed to beam break mode

--- src/main/java/org/steelhawks/subsystems/claw/Claw.java
+++ src/main/java/org/steelhawks/subsystems/claw/Claw.java
@@ -13,7 +13,7 @@ public class Claw extends SubsystemBase {
 
     private static final double CURRENT_THRESHOLD = 30;
     private static final double INTAKE_SPEED = 0.05;
-    private static final double DIST_TO_HAVE_CORAL = 0.075;
+    public static final double DIST_TO_HAVE_CORAL = 0.075;
     private boolean isIntaking = false;
 
     private final ClawIntakeIOInputsAutoLogged inputs = new ClawIntakeIOInputsAutoLogged();
@@ -26,7 +26,7 @@ public class Claw extends SubsystemBase {
                 new Trigger(
                     () -> inputs.currentAmps > CURRENT_THRESHOLD && isIntaking);
             case HAWKRIDER -> new Trigger(() -> false);
-            default -> new Trigger(() -> beamDebounce.calculate(inputs.beamDistance < DIST_TO_HAVE_CORAL));
+            default -> new Trigger(() -> beamDebounce.calculate(inputs.beamBroken));
         };
     }
 
