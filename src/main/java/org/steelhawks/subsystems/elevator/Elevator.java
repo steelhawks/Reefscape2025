82088b9f72bfb30e078ac45d7f719aa75bb53646
Dylan Wang <146579075+DBHJ@users.noreply.github.com>
2025-03-27 18:03:43 -0400

    Update at 'Thu Mar 27 18:03:43 EDT 2025'

--- src/main/java/org/steelhawks/subsystems/elevator/Elevator.java
+++ src/main/java/org/steelhawks/subsystems/elevator/Elevator.java
@@ -161,7 +161,7 @@ public class Elevator extends SubsystemBase {
 
     @AutoLogOutput(key = "Elevator/AdjustedPosition")
     public double getPosition() {
-        return inputs.encoderPositionRad + ElevatorConstants.CANCODER_OFFSET;
+        return inputs.encoderPositionRad - ElevatorConstants.CANCODER_OFFSET;
     }
 
     public Trigger atGoal() {
