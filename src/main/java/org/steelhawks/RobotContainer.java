ac2e63a3091301aca032bea97895c372f49e9a6e
Dylan Wang <146579075+DBHJ@users.noreply.github.com>
2025-03-27 17:39:23 -0400

    Update at 'Thu Mar 27 17:39:22 EDT 2025'

--- src/main/java/org/steelhawks/RobotContainer.java
+++ src/main/java/org/steelhawks/RobotContainer.java
@@ -530,8 +530,7 @@ public class RobotContainer {
                     s_Claw.shootCoral(),
                     () ->
                         (s_Elevator.getDesiredState() == ElevatorConstants.State.L1.getRadians() ||
-                            (s_Elevator.getDesiredState() == ElevatorConstants.State.L4.getRadians()) && s_Elevator.isEnabled()) ||
-                                s_Elevator.getDesiredState() == ElevatorConstants.State.L3.getRadians())
+                            (s_Elevator.getDesiredState() == ElevatorConstants.State.L4.getRadians()) && s_Elevator.isEnabled()))
                 .alongWith(LED.getInstance().flashCommand(LEDColor.WHITE, 0.2, 2.0).repeatedly()));
 
         operator.povLeft()
