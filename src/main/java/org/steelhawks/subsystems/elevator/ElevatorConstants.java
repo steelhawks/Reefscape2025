bbbc0314870657f05472935e85493dd92601992f
Dylan Wang <146579075+DBHJ@users.noreply.github.com>
2025-03-28 15:41:42 -0400

    Update at 'Fri Mar 28 15:41:42 EDT 2025'

--- src/main/java/org/steelhawks/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/org/steelhawks/subsystems/elevator/ElevatorConstants.java
@@ -120,8 +120,8 @@ public class ElevatorConstants {
                 KG = 0.176;
                 KV = (((2.0 - 1.0) / (4.086524818927348 - 1.8346410223112268)) + ((1.0 - 0.5) / (1.8346410223112268 - 0.6381360077604268))) / 2.0;
                 KP = 5.5;
-                KI = 0.01;
-                KD = 0.2;
+                KI = 0.02;
+                KD = 0.21;
                 MAX_VELOCITY_PER_SEC = 40; // 35
                 MAX_ACCELERATION_PER_SEC_SQUARED = 60; // was 70
                 TOLERANCE = Units.degreesToRadians(5);
