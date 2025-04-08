587199efc5893e57fe8ec229b03b81b6c85b48bc
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2025-04-07 20:38:19 -0400

    add algae claw

---
+++ src/main/java/org/steelhawks/subsystems/algaeclaw/AlgaeClawIO.java
@@ -0,0 +1,46 @@
+package org.steelhawks.subsystems.algaeclaw;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface AlgaeClawIO {
+
+    @AutoLog
+    class AlgaeClawIOInputs {
+        public double goal = 0.0;
+
+        public boolean pivotConnected = false;
+        public double pivotPosition = 0.0;
+        public double pivotVelocity = 0.0;
+        public double pivotAppliedVolts = 0.0;
+        public double pivotCurrent = 0.0;
+        public double pivotTemperature = 0.0;
+
+        public boolean spinConnected = false;
+        public double spinPosition = 0.0;
+        public double spinVelocity = 0.0;
+        public double spinAppliedVolts = 0.0;
+        public double spinCurrent = 0.0;
+        public double spinTemperature = 0.0;
+
+        public boolean encoderConnected = false;
+        public double encoderPosition = 0.0;
+        public double encoderVelocity = 0.0;
+        public double encoderAppliedVolts = 0.0;
+    }
+
+    default void updateInputs(AlgaeClawIOInputs inputs) {}
+
+    default void runSpin(double speed) {}
+
+    default void stopSpin() {}
+
+    default void runPivot(double volts) {}
+
+    default void stopPivot() {}
+
+    default void runPosition(double positionRad) {}
+
+    default void setPID(double kP, double kI, double kD) {}
+
+    default void setFF(double kS, double kG, double kV) {}
+}
--- src/main/java/org/steelhawks/util/ArmDriveFeedforward.java
+++ src/main/java/org/steelhawks/util/ArmDriveFeedforward.java
@@ -6,31 +6,29 @@ import java.util.function.DoubleSupplier;
  * This class calculates the feedforward for an arm to overcome the acceleration due to the robot's movement.
  * It uses kG and the robot relative acceleration in Gs to compute the necessary voltage.
  *
- * @author farhanjamil
+ * @author Farhan Jamil
  */
 public class ArmDriveFeedforward {
 
-    private final DoubleSupplier xAccelInGs;
     private final double kG;
 
     /**
      * Creates a new ArmDriveFeedforward.
      *
      * @param kG the volts to hold the arm up
-     * @param xAccelInGs the acceleration in Gs
      */
-    public ArmDriveFeedforward(double kG, DoubleSupplier xAccelInGs) {
+    public ArmDriveFeedforward(double kG) {
         this.kG = kG;
-        this.xAccelInGs = xAccelInGs;
     }
 
     /**
      * Calculates the feedforward voltage based on the current measurement.
      *
      * @param measurement the current angle of the arm in radians
+     * @param xAccelInGs the acceleration in Gs
      * @return the calculated feedforward voltage
      */
-    public double calculate(double measurement) {
+    public double calculate(double measurement, DoubleSupplier xAccelInGs) {
         return -(kG * Math.sin(measurement) * xAccelInGs.getAsDouble());
     }
 }
