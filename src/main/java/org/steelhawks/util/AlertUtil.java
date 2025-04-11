cf0c3d1d823db2aced08a7781c93690248b7ee12
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2025-04-11 19:31:39 -0400

    add single updater for alertutil

--- src/main/java/org/steelhawks/util/AlertUtil.java
+++ src/main/java/org/steelhawks/util/AlertUtil.java
@@ -2,18 +2,27 @@ package org.steelhawks.util;
 
 import edu.wpi.first.wpilibj.Alert;
 
+import java.util.ArrayList;
 import java.util.function.BooleanSupplier;
 
-public final class AlertUtil extends VirtualSubsystem {
+public final class AlertUtil {
 
-    private static int instances = 0;
     private BooleanSupplier condition;
     private final Alert alert;
 
+    private static final ArrayList<AlertUtil> alerts = new ArrayList<>();
+    private static final VirtualSubsystem updater = new VirtualSubsystem("AlertUtil") {
+        @Override
+        public void periodic() {
+            for (AlertUtil alertUtil : alerts) {
+                alertUtil.update();
+            }
+        }
+    };
+
     public AlertUtil(String message, Alert.AlertType alertType) {
-        super("AlertUtil/" + instances);
-        instances++;
         alert = new Alert(message, alertType);
+        alerts.add(this);
     }
 
     public Alert withCondition(BooleanSupplier condition) {
@@ -21,9 +30,15 @@ public final class AlertUtil extends VirtualSubsystem {
         return alert;
     }
 
+    public boolean isActive() {
+        return alert.get();
+    }
 
-    @Override
-    public void periodic() {
+    public void updateCondition(BooleanSupplier condition) {
+        this.condition = condition;
+    }
+
+    private void update() {
         alert.set(condition != null && condition.getAsBoolean());
     }
 }
