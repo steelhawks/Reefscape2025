4defccdae0e02f896da0f61e37ae183727c95156
Dylan Wang <146579075+DBHJ@users.noreply.github.com>
2025-04-16 19:26:21 -0500

    Update at 'Wed Apr 16 19:26:21 CDT 2025'

--- src/main/java/org/steelhawks/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/org/steelhawks/subsystems/elevator/ElevatorConstants.java
@@ -49,7 +49,7 @@ public class ElevatorConstants {
         }
     }
 
-    public static double CANCODER_OFFSET = -0.36328125; // in rotations
+    public static double CANCODER_OFFSET = -0.36474609375; // in rotations
 
     public static final int LIMIT_SWITCH_ID;
     public static final int LEFT_ID;
