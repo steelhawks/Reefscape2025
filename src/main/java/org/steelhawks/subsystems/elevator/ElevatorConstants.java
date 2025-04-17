2ea01de7e836fe1710f1ea04584905f62236b90b
Dylan Wang <146579075+DBHJ@users.noreply.github.com>
2025-04-17 07:50:57 -0500

    Update at 'Thu Apr 17 07:50:57 CDT 2025'

--- src/main/java/org/steelhawks/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/org/steelhawks/subsystems/elevator/ElevatorConstants.java
@@ -9,8 +9,8 @@ import java.util.Arrays;
 public class ElevatorConstants {
 
     public enum State {
-        L4(59.905784718904, 23.299634187195004, Units.rotationsToRadians(3.0)), // Before claw raise: 24.21
-        L3(35.3237425930366, 13.956157208183564, Units.rotationsToRadians(2.0)), // Slightly too high: 14.394875713518857 // Before claw raise: 14.947108797157687
+        L4(59.905784718904, 22.01569226773472, Units.rotationsToRadians(3.0)), // Before claw raise: 24.21
+        L3(35.3237425930366, 13.048040581755263, Units.rotationsToRadians(2.0)), // Slightly too high: 14.394875713518857 // Before claw raise: 14.947108797157687
         L2(19.376478322177476, 6.958136853849268, Units.rotationsToRadians(1.0)), // Before claw raise: 9.10417597610128
         L1(11.3936423020206, 4.947855031325136, Units.rotationsToRadians(0.5)),
         HOME(0.0, 0.0, 0.0),
