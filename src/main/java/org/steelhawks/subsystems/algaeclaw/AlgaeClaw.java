5b734cdcf7dee3c3993aee3456063142653370c0
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2025-07-23 19:40:41 -0400

    fixes to toggle logic + fused cancoder for algaeclaw

--- src/main/java/org/steelhawks/subsystems/algaeclaw/AlgaeClaw.java
+++ src/main/java/org/steelhawks/subsystems/algaeclaw/AlgaeClaw.java
@@ -14,7 +14,6 @@ import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 import org.steelhawks.*;
 import org.steelhawks.subsystems.LED;
-import org.steelhawks.subsystems.elevator.ElevatorConstants;
 import org.steelhawks.util.LoggedTunableNumber;
 import org.steelhawks.util.LoopTimeUtil;
 
@@ -69,15 +68,6 @@ public class AlgaeClaw extends SubsystemBase {
         io.updateInputs(inputs);
         Logger.processInputs("AlgaeClaw", inputs);
 
-        if (Toggles.tuningMode.get()) {
-            LoggedTunableNumber.ifChanged(this.hashCode(), () -> {
-                io.setPID(
-                    AlgaeClawConstants.PIVOT_KP.get(),
-                    AlgaeClawConstants.PIVOT_KI.get(),
-                    AlgaeClawConstants.PIVOT_KD.get());
-            }, AlgaeClawConstants.PIVOT_KP, AlgaeClawConstants.PIVOT_KI, AlgaeClawConstants.PIVOT_KD);
-        }
-
         shouldEStop =
             (inputs.pivotPosition >= AlgaeClawConstants.MAX_PIVOT_RADIANS && Math.signum(velocityFilter.calculate(inputs.encoderPosition)) == 1)
                 || (inputs.pivotPosition <= AlgaeClawConstants.MIN_PIVOT_RADIANS && Math.signum(velocityFilter.calculate(inputs.encoderVelocity)) == -1); // prob need to run a deadband for small movements
@@ -101,14 +91,20 @@ public class AlgaeClaw extends SubsystemBase {
                 if (pivotVolts == null) {
                     pivotVolts = new LoggedTunableNumber("AlgaeClaw/PivotVolts", 0.0);
                 }
+                io.runPivot(pivotVolts.get());
             }
-            io.runPivot(pivotVolts.get());
             if (Toggles.AlgaeClaw.toggleCurrentOverride.get()) {
                 if (pivotAmps == null) {
-                    pivotAmps = new LoggedTunableNumber("AlgaeClaw/CurrentVolts", 0.0);
+                    pivotAmps = new LoggedTunableNumber("AlgaeClaw/CurrentAmps", 0.0);
                 }
+                io.runPivotOpenLoop(pivotAmps.get());
             }
-            io.runPivot(pivotVolts.get());
+            LoggedTunableNumber.ifChanged(this.hashCode(), () -> {
+                io.setPID(
+                    AlgaeClawConstants.PIVOT_KP.get(),
+                    AlgaeClawConstants.PIVOT_KI.get(),
+                    AlgaeClawConstants.PIVOT_KD.get());
+            }, AlgaeClawConstants.PIVOT_KP, AlgaeClawConstants.PIVOT_KI, AlgaeClawConstants.PIVOT_KD);
         }
         if (shouldRun) {
             double previousVelocity = setpoint.velocity;
@@ -199,22 +195,25 @@ public class AlgaeClaw extends SubsystemBase {
                 goal = new TrapezoidProfile.State(inputs.goal, 0.0);
                 desiredGoal = state;
             }, this)
-        .withName("Set Desired State");
+        .withName("Set Desired State")
+        .ignoringDisable(true);
     }
 
     public Command pivotManual(DoubleSupplier rightAxis) {
-        return Commands.run(() -> {
-            if (hasAlgae()) {
-                io.runSpin(AlgaeClawConstants.RETAIN_ALGAE_SPEED);
-            } else {
-                io.stopSpin();
-            }
-            double speed = AlgaeClawConstants.PIVOT_KG.get() / 12.0
-                + MathUtil.clamp(rightAxis.getAsDouble(),
-                    -AlgaeClawConstants.MAX_MANUAL_SPEED,
-                    AlgaeClawConstants.MAX_MANUAL_SPEED);
-            io.runPivotViaSpeed(speed);
-        }, this);
+        return Commands.runOnce(() -> isManual = true)
+            .andThen(
+                Commands.run(() -> {
+                    if (hasAlgae()) {
+                        io.runSpin(AlgaeClawConstants.RETAIN_ALGAE_SPEED);
+                    } else {
+                        io.stopSpin();
+                    }
+                    double speed = AlgaeClawConstants.PIVOT_KG.get() / 12.0
+                        + MathUtil.clamp(rightAxis.getAsDouble(),
+                            -AlgaeClawConstants.MAX_MANUAL_SPEED,
+                            AlgaeClawConstants.MAX_MANUAL_SPEED);
+                    io.runPivotViaSpeed(speed);
+                }, this));
     }
 
     public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
